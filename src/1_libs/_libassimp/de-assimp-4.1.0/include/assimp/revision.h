5b243ec2a53152aaa9cceb67aa63af7921ab9334
Benjamin Hampe <benjaminhampe@gmx.de>
2025-09-28 02:26:12 +0200

    Moved common libs for to be Surge-XT and Rack2 plugins.

--- src/1_libs/_libassimp/de-assimp-4.1.0/include/assimp/revision.h
+++ src/1_libs/_libassimp/de-assimp-4.1.0/include/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x3093fd94
+#define GitVersion 0xf709b355
 #define GitBranch "main"
 
 #endif // ASSIMP_REVISION_H_INC
